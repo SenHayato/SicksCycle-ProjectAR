13abc9d5161e44c0a4d84a5b8ab2e68857be7631
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-05-22 11:00:33 +0700

    Test Sistem VA Story Telling
    2 Sistem yang masih dicoba
    - Sistem voice play back mirip dengan sistem visual novel
    - Sistem subtitle mirip dengan file SRT dalam film

---
+++ Project AR Burung/Assets/Scripts/Dialog/DialogueSystem.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[Serializable]
+public class DialogueLine //Sesuaikan dialog, banyak dialog, dan voice playback
+{
+    [TextArea] public string text;
+    public AudioClip voiceClip;
+}
+
+public class DialogueSystem : MonoBehaviour
+{
+    [Header("Dialogue List")]
+    [SerializeField] List<DialogueLine> dialogues;
+    int currentIndex = 0;
+
+    [Header("Dialogue Component")]
+    [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] AudioSource dialogueAudio;
+
+    private void Start()
+    {
+        StartCoroutine(PlayDialogue());
+    }
+
+    IEnumerator PlayDialogue()
+    {
+        while (currentIndex < dialogues.Count)
+        {
+            DialogueLine line = dialogues[currentIndex];
+
+            dialogueText.text = line.text;
+
+            dialogueAudio.clip = line.voiceClip;
+            dialogueAudio.Play();
+
+            // jeda ampek voice playback selesai
+            yield return new WaitForSeconds(line.voiceClip.length);
+
+            currentIndex++;
+        }
+    }
+}
