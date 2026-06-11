2185539f6f74a164886155a7ccf375711b7e6cf3
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-06-12 00:03:54 +0700

    Button Size Up

--- Project AR Burung/Assets/Scripts/Admin/ButtonADV.cs
+++ Project AR Burung/Assets/Scripts/Admin/ButtonADV.cs
@@ -8,16 +8,25 @@ using UnityEngine.UI;
 public class ButtonADV : Button
 {
     [SerializeField] Color transparent = new Color(255f, 255f, 255f, 0f);
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] Vector2 normalSize;
+    [SerializeField] float sizeUp;
 
     protected override void Start()
     {
-       targetGraphic.color = transparent; 
+        targetGraphic.color = transparent;
+        rectTransform = GetComponent<RectTransform>();
+        normalSize = rectTransform.sizeDelta;
     }
 
     public override void OnPointerEnter(PointerEventData eventData)
     {
         base.OnPointerEnter(eventData);
         targetGraphic.DOColor(Color.white, 0.15f).SetEase(Ease.OutSine);
+        if (sizeUp > 0f)
+        {
+            rectTransform.DOSizeDelta(normalSize * sizeUp, 0.15f).SetEase(Ease.OutSine);
+        }
         //Debug.Log("Button Masuk");
     }
 
@@ -25,6 +34,10 @@ public class ButtonADV : Button
     {
         base.OnPointerExit(eventData);
         targetGraphic.DOColor(transparent, 0.15f).SetEase(Ease.OutSine);
+        if (sizeUp > 0f)
+        {
+            rectTransform.DOSizeDelta(normalSize, 0.15f).SetEase(Ease.OutSine);
+        }
         //Debug.Log("Button Exit");
     }
 }
