5d1f5eec7c664fbc6c0be67113b8538c883d8d79
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-06-11 23:40:24 +0700

    Button Menu, Button sound, dan Menu BGM

--- Project AR Burung/Assets/Scripts/Admin/AdminActive.cs
+++ Project AR Burung/Assets/Scripts/Admin/AdminActive.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class AdminActive : MonoBehaviour
 {
+    public static AdminActive instance {  get; private set; }
+
     //tambah fitur max frame rate dan keluar aplikasi
     [SerializeField] int maxFrameRate;
     [SerializeField] string menuScene;
+    [SerializeField] AudioSource buttonSFX;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
     void Start()
     {
         Application.targetFrameRate = maxFrameRate;
@@ -16,6 +32,11 @@ public class AdminActive : MonoBehaviour
     public void BackToMenu()
     {
         SceneManager.LoadSceneAsync(menuScene);
+    }
+
+    public void PlayButtonSound()
+    {
+        instance.buttonSFX.PlayOneShot(buttonSFX.clip);
     }
 
     void Update()
--- Project AR Burung/Assets/Scripts/Admin/MenuManager.cs
+++ Project AR Burung/Assets/Scripts/Admin/MenuManager.cs
@@ -5,12 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public static MenuManager Instance { get; private set; }
+
+    [Header("Menu Component")]
     [SerializeField] string ARScene;
     [SerializeField] GameObject creditPanel;
+
+    [Header("Menu Button Component")]
+    [SerializeField] GameObject ExitButton;
+    [SerializeField] GameObject CreditButton;
+    [SerializeField] GameObject ScanButton;
+
+    [Header("Button Audio")]
+    [SerializeField] AudioSource buttonSFX;
+
+    bool menuButtonToggle = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(Instance);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     private void Start()
     {
         creditPanel.SetActive(false);
+        MenuButtonToggler();
     }
 
     public void LoadtoAR()
@@ -23,6 +49,27 @@ public class MenuManager : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("APP Quit");
+    }
+
+    public void ButtonSoundPlay()
+    {
+        Instance.buttonSFX.PlayOneShot(buttonSFX.clip);
+    }
+
+    public void MenuButtonToggler()
+    {
+        if (menuButtonToggle)
+        {
+            menuButtonToggle = false;
+        }
+        else
+        {
+            menuButtonToggle = true;
+        }
+
+        ExitButton.SetActive(menuButtonToggle);
+        CreditButton.SetActive(menuButtonToggle);
+        ScanButton.SetActive(menuButtonToggle);
     }
 
     private void Update()
